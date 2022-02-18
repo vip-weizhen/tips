7436fd8338824fa704c82ba332d5acf61554fc4c
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2022-02-18 09:00:38 +0800

    Update Next_work.py

--- Next_work.py
+++ Next_work.py
@@ -10,12 +10,11 @@ import requests, json
 import datetime
 import time
 
-wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=37e7e521-28b2-4064-b572-8effb4071d6a"  # 测试机器人1号
+wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=37e7e521-28b2-4064-b572-8effb4071d6a"
 send_message = "测试：测试机器人1号………………………………！"
 
 
 def send_msg(content):
-  """艾特全部，并发送指定信息"""
   data = json.dumps({"msgtype": "text", "text": {"content": content, "mentioned_list":["@all"]}})
   r = requests.post(wx_url, data, auth=('Content-Type', 'application/json'))
   print(r.json)
