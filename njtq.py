87ce3f359f1973cb3e47921df1bf864447fc71a0
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2022-03-10 12:43:14 +0800

    Create njtq.py

---
+++ njtq.py
@@ -0,0 +1,16 @@
+#! -*- coding: utf-8 -*-
+"""
+Author: Mr.wei
+Create type_time: 2022-3-10
+Info: 定期向企业微信推送消息
+cron: 00 7 * * *
+new Env('天气预报');
+"""
+
+import requests, json
+wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=37e7e521-28b2-4064-b572-8effb4071d6a"
+def send_msg(content):
+  data = json.dumps({"msgtype": "news", "articles": [{"url":"https://tianqiapi.com/api.php?style=tw&skin=pitaya"}]})
+  r = requests.post(wx_url, data, auth=('Content-Type', 'application/json'))
+  print(r.json)
+send_msg(requests)
