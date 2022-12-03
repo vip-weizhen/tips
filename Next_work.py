22407aceb86c3da5dde816d60a86e8ea51b166e6
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2022-12-03 17:24:48 +0800

    Update Next_work.py

--- Next_work.py
+++ Next_work.py
@@ -6,7 +6,7 @@ Info: 定期向企业微信推送消息
 cron: 20 17 * * *
 new Env('工作报备');
 """
-import requests, json
+import requests, json, time
 
 wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2af5db49-c10e-4d65-8acd-0aeb038a5502"
 send_message = "报备次日工作~"
@@ -18,5 +18,7 @@ def send_msg(content):
   print(r.json)
   
 send_msg(send_message)
+time.sleep(5)
 send_msg(send_message)
+time.sleep(5)
 send_msg(send_message)
