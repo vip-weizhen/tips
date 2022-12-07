e895cd360c35f9c11cbd202cf677d9aa9bd34eb5
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2022-12-07 17:21:47 +0800

    Update Next_work.py

--- Next_work.py
+++ Next_work.py
@@ -9,7 +9,7 @@ new Env('工作报备');
 import requests, json, time
 
 wx_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=2af5db49-c10e-4d65-8acd-0aeb038a5502"
-send_message = "报备次日工作~"
+send_message = "报备次日工作计划~"
 
 
 def send_msg(content):
