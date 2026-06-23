957b9461a4ea89103aefa11ce1d8013d0815db6c
AshenPeak <47912513+vip-weizhen@users.noreply.github.com>
2026-06-23 13:11:29 +0800

    Update sendNotify.py

--- sendNotify.py
+++ sendNotify.py
@@ -33,7 +33,7 @@ DD_BOT_SECRET = ''          # 钉钉机器人的DD_BOT_SECRET; secrets可填
 QQ_SKEY = ''                # qq机器人的QQ_SKEY; secrets可填
 QQ_MODE = ''                # qq机器人的QQ_MODE; secrets可填
 QYWX_AM = ''                # 企业微信
-QYWX_KEY = 'ed9300fc-f7dd-443a-90d5-ab7fa1afa172'                # 企业微信BOT
+QYWX_KEY = ''                # 企业微信BOT
 PUSH_PLUS_TOKEN = ''        # 微信推送Plus+
 
 notify_mode = []
